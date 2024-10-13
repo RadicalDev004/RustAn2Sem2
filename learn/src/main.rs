fdc286671dd42dd8a7dc70a103c5df7beffdefea
RadicalDev004 <99879583+RadicalDev004@users.noreply.github.com>
2024-10-13 14:31:46 +0300

    Week2 lab

--- learn/src/main.rs
+++ learn/src/main.rs
@@ -1,51 +1,129 @@
-fn is_prime(n: u32) -> bool {
-    if n < 2 {
-        return false;
-    }
-    for i in 2..=((n as f64).sqrt() as u32) {
-        if n % i == 0 {
-            return false;
-        }
-    }
-    true
-}
-
-fn gcd(a: u32, b: u32) -> u32 {
-    if b == 0 {
-        a
-    } else {
-        gcd(b, a % b)
-    }
-}
-
-fn are_coprime(a: u32, b: u32) -> bool {
-    gcd(a, b) == 1
-}
-
-fn sing_bottles() {
-    for i in (1..=99).rev() {
-        let bottle_word = if i == 1 { "bottle" } else { "bottles" };
-        let next_bottle = if i == 1 { "no more bottles" } else { &format!("{} bottles", i - 1)[..] }; // Using slicing to get &str
-
-        println!("{} {} of beer on the wall, {} {} of beer.", i, bottle_word, i, bottle_word);
-        println!("Take one down, pass it around, {} of beer on the wall.\n", next_bottle);
-    }
-    println!("No more bottles of beer on the wall.");
-    println!("Go to the store and buy some more, 99 bottles of beer on the wall.");
-}
-
 fn main() {
-    for i in 0..100 {
-        if is_prime(i) {
-            println!("{}", i);
+    // let mut s = String::from("");
+    // let mut i = 0;
+    // while i < 26 {
+    //     let c = (i as u8 + 'a' as u8) as char;
+    //     add_chars_n_ref(&mut s, c, 26-i);
+    //     s = add_chars_n(s, c, 26 - i);
+
+    //     i += 1;
+    // }
+
+    // print!("{}", s);
+
+    let mut S : String = String::from("");
+
+    add_space(&mut S, 40);
+    add_str(&mut S, "I");
+    add_space(&mut S, 1);
+    add_str(&mut S, "💚\n");
+
+    add_space(&mut S, 40);
+    add_str(&mut S, "RUST\n");
+
+    add_space(&mut S, 4);
+    add_str(&mut S, "Most");
+    add_space(&mut S, 12);
+    add_str(&mut S, "crate");
+    add_space(&mut S, 6);
+    add_integer(&mut S, 306437968);
+    add_space(&mut S, 11);
+    add_str(&mut S, "and");
+    add_space(&mut S, 5);
+    add_str(&mut S, "latest");
+    add_space(&mut S, 9);
+    add_str(&mut S, "is\n");
+
+    add_space(&mut S, 9);
+    add_str(&mut S, "downloaded");
+    add_space(&mut S, 8);
+    add_str(&mut S, "has");
+    add_space(&mut S, 13);
+    add_str(&mut S, "downloads");
+    add_space(&mut S, 5);
+    add_str(&mut S, "the");
+    add_space(&mut S, 8);
+    add_str(&mut S, "version");
+    add_space(&mut S, 4);
+    add_float(&mut S, 2.038, 3);
+    add_str(&mut S, ".");
+
+    println!("{}", S);
+}
+
+fn add_chars_n(s : String, c : char, n : u32) -> String {
+    let mut st : String = String::from(s);
+    for j in 1..=n {
+        st.push(c);
+    }
+    st
+}
+
+fn add_chars_n_ref(s : &mut String, c : char, n : u32) {
+    for j in 1..=n {
+        s.push(c);
+    }
+}
+
+fn add_space(s : &mut String, n : u32) {
+    for j in 1..=n {
+        s.push(' ');
+    }
+}
+
+fn add_str2(s : &mut String, st : String) {
+    s.push_str(&st[0..st.len()]);
+}
+
+fn add_str(s : &mut String, st : &str) {
+    s.push_str(st);
+}
+
+fn add_integer(s : &mut String, i : u32) {
+    let mut tmp : String = String::from("");
+    let mut cnt = 0;
+    let mut j = i;
+
+    while j > 0 {
+        tmp.push(('0' as u8 + (j%10) as u8) as char);
+        cnt+=1;
+        
+        j/=10;
+
+        if cnt % 3 == 0 && j > 0 {
+            tmp.push('_');
         }
     }
-    for i in 0..100 {
-        for j in 0..100 {
-            if i != j && are_coprime(i, j) {
-                println!("({}, {})", i, j);
-            }
+
+    for k in (0..tmp.len()).rev() {
+        s.push(tmp.chars().nth(k).unwrap());
+    }
+}
+
+fn add_float(s : &mut String, i : f32, mut prc : i32) {
+    let mut tmp : String = String::from("");
+    let mut j = i as u32;
+
+    //before .
+    while j > 0 {
+        tmp.push(('0' as u8 + (j%10) as u8) as char);
+        j/=10;
+    }
+
+    for k in (0..tmp.len()).rev() {
+        s.push(tmp.chars().nth(k).unwrap());
+    }
+    s.push('.');
+
+    //after .
+    let mut l = i;
+
+    while l != (l as u32) as f32 {
+        l *= 10 as f32;
+        s.push(('0' as u8 + (l as u32 %10) as u8) as char);
+        prc = prc -1;
+        if prc == 0 {
+            break;
         }
     }
-    sing_bottles();
 }
